c7bd53371f3921106c7e6b79f710d0670bcf5e8a
ENZO GAMBATTO ALMEIDA <1134927@atitus.local>
2026-04-22 19:34:23 -0300

    Persist requesters in database

--- models.py
+++ models.py
@@ -26,3 +26,14 @@ class Comentario(db.Model):
     autor = db.Column(db.Text)
     data = db.Column(db.Text)
 
+
+class Requester(db.Model):
+    __tablename__ = 'requesters'
+    __table_args__ = {'sqlite_autoincrement': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False, unique=True)
+    cargo = db.Column(db.Text, nullable=False)
+    data_criacao = db.Column(db.Text, nullable=False)
+
